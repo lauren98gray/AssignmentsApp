00d7582ea94bf7199964237d47e5421b8c6e0df8
Lauren Gray <lauren98gray@gmail.com>
2018-02-27 18:15:58 -0600

    #153893472 Output current date-time

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("\n\nHello, AssignmentsApp!\n");
+
+        // Output current date-time
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("The current date-time is " + current);
     }
 }
