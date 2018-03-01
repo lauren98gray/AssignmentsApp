ff35f3f3f2b8eec68befa8e60dbb22a3eb3e823e
Lauren Gray <lauren98gray@gmail.com>
2018-03-01 12:53:48 -0600

    #153893475 Output tomorrow's date using a formatter

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
@@ -9,5 +10,16 @@ public class Main {
         // Output current date-time
         LocalDateTime current = LocalDateTime.now();
         System.out.println("The current date-time is " + current);
+
+        // Output tomorrow's date using a formatter
+        String formattedTomorrow = formatTomorrow(current);
+        System.out.println("Tomorrow's date is " + formattedTomorrow);
+    }
+
+    private static String formatTomorrow(LocalDateTime today) {
+        LocalDateTime tomorrow = today.plusDays(1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm:ss");
+        String formatDateTime = tomorrow.format(formatter);
+        return formatDateTime;
     }
 }
