b2483bd980ad9e618658c75c42b16cee488a2d84
Lauren Gray <lauren98gray@gmail.com>
2018-03-08 13:48:10 -0600

    #153893600 Store data from the file into an ArrayList of LocalDateTime objects

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
+
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -43,24 +45,35 @@ public class Main {
         System.out.println("\nThere are " + numDaysBetween + " days between " + current + " and " + fiveWeeksAdded + ".");
 
         // Given two dates, output the earlier
-        // not sure if meant to use each of the suggestions or just choose one. just in case, I'm doing them all
-        //using isBefore method
-        LocalDateTime earlyDateWithIsBefore = findEarlierDateWithIsBefore(current, fiveWeeksAdded);
-        System.out.println("\nEarlier date using isBefore method: " + earlyDateWithIsBefore);
-        //using isAfter method
-        LocalDateTime earlyDateWithIsAfter = findEarlierDateWithIsAfter(current, fiveWeeksAdded);
-        System.out.println("Earlier date using isAfter method: " + earlyDateWithIsAfter);
-        //using compareTo
-        LocalDateTime earlyDateWithCompareTo = findEarlierDateWithCompareTo(current, fiveWeeksAdded);
-        System.out.println("Earlier date using compareTo method: " + earlyDateWithCompareTo);
+        LocalDateTime earlierDate = findEarlierDate(current, fiveWeeksAdded);
+        System.out.println("\nEarlier date: " + earlierDate);
 
         // Create a file with 100 random "month/day/year hour:minutes" on each line
         ArrayList<String> listOfDates = generateRandomDates(100);
         createFile("randomDates.txt", listOfDates);
 
+        // Store data from the file into an ArrayList of LocalDateTime objects
+        ArrayList<LocalDateTime> dataInFile = createArrayListFromFileDate("randomDates.txt");
+        System.out.println("\nArrayList of LocalDateTime objects in file: " + dataInFile);
 
 
+    }
 
+    private static ArrayList<LocalDateTime> createArrayListFromFileDate(String fileName) throws FileNotFoundException {
+        File infile = new File(fileName);
+        ArrayList<LocalDateTime> data = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yy HH:mm");
+        if (!infile.exists()){
+            System.out.println("Oh no, you can't read from a file that doesn't exist!");
+        } else {
+            try(Scanner sc = new Scanner(infile)) {
+                while (sc.hasNext()){
+                    LocalDateTime name = LocalDateTime.parse(sc.nextLine(), formatter);
+                    data.add(name);
+                }
+            }
+        }
+        return data;
     }
 
     private static void createFile(String fileName, ArrayList<String> dataToAddToFile) throws FileNotFoundException {
@@ -92,26 +105,7 @@ public class Main {
         return answer;
     }
 
-
-    private static LocalDateTime findEarlierDateWithCompareTo(LocalDateTime date1, LocalDateTime date2) {
-        if (date1.compareTo(date2) < 0) {
-            return date1;
-        } else if (date1.compareTo(date2) > 0) {
-            return date2;
-        } else {
-            return null;
-        }
-    }
-
-    private static LocalDateTime findEarlierDateWithIsAfter(LocalDateTime date1, LocalDateTime date2) {
-        if (date1.isAfter(date2)) {
-            return date2;
-        } else {
-            return date1;
-        }
-    }
-
-    private static LocalDateTime findEarlierDateWithIsBefore(LocalDateTime date1, LocalDateTime date2) {
+    private static LocalDateTime findEarlierDate(LocalDateTime date1, LocalDateTime date2) {
         if (date1.isBefore(date2)) {
             return date1;
         } else {
